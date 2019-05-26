12f3286290f8bdfe9efc110e3c35d8e840050be5
DanielArmengod <danielarmengodsorrosal@gmail.com>
2019-05-26 19:19:53 +0200

    Subida de bot terminado, pero falta pulirlo bastante

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ class Tamagotchi{
     }
 }
 
+// last Id from bot
+var lastId = '';
+
+
+
 // url bot
-var urlBot = 'https://api.telegram.org/bot744288029:AAHikTZ2Esa_wejAvdHp0OkMNSwjOxRc6m8';
+var urlBot = 'https://api.telegram.org/bot744288029:AAHyh3PoGODKsvB6jIZ1WUJEm8j8IvN_KlE/getUpdates';
 
 var tama = new Tamagotchi(0, 100, 100, 0);
 
@@ -24,15 +29,7 @@ var https = require('https');
 
 
 // start the loop for the telegram api
-setInterval( updateStatus, 2000);
-
-/**
- * @description function that
- * @method GET
- */
-function info() {
-    var resp = 'Welcome to the tamagotchi time..., that are the commands...: /hungry, /life, /happiness and /sleepness';
-}
+var intervl = setInterval( updateStatus, 5000);
 
 /**
  * @description function feed, minus the hungry level
@@ -63,8 +60,6 @@ function updateStatus(){
 
     // get the last message from the bot of telegram
     getResponseFromServer();
-    // print in console the status from the tamagotchi
-    logTamaStatus();
 
     //main status changed
     tama.hungry     = ( tama.hungry < 100 ) ? tama.hungry += 10 : tama.hungry;
@@ -76,7 +71,7 @@ function updateStatus(){
     tama.life = tama.happiness == 0 && tama.life > 0 ? tama.life -= 10 : tama.life;
     tama.life = tama.sleepness == 100 && tama.life > 0 ? tama.life -= 10 : tama.life;
 
-    logTamaStatus();
+    //logTamaStatus();
 
     //check life status
     if( tama.life == 0 ){
@@ -84,8 +79,34 @@ function updateStatus(){
     }
     
 }
+
 /**
- * @description Query call to get the bot status
+ * @description get message from the bot
+*/
+function sendMessage( idChat, textChat ){
+    var urlSendBot = `https://api.telegram.org/bot744288029:AAHyh3PoGODKsvB6jIZ1WUJEm8j8IvN_KlE/sendMessage?chat_id=${idChat}&text=${textChat}`;
+
+    
+    https.get(urlSendBot, (resp) => {
+    
+        let data = '';
+    
+        resp.on( 'data', (chunck) => {
+            data += chunck;
+        });
+    
+        resp.on( 'end', () => {
+            console.log('mensaje enviado');
+        });
+    
+    }).on('error', (err) => {
+        console.log( 'Error : ' + err.message );
+    });
+}
+
+
+/**
+ * @description get message from the bot
 */
 function getResponseFromServer(){
     https.get(urlBot, (resp) => {
@@ -97,13 +118,70 @@ function getResponseFromServer(){
         });
     
         resp.on( 'end', () => {
-            console.log( 'INFO RESPUESTA SERVIDOR : ' + data );
             // se va a deserializar y recoger el comando de dicho bot
+            var body = JSON.parse(data);
+            //se recoge el mensaje del bot
+            console.log(':::::: Mensaje ::::::');
+            
+            var command = body.result[body.result.length-1].message.text;
+            if(lastId != body.result[body.result.length-1].update_id ){
+                console.log( 'nuevo comando detectado : ' + command );
+
+                var text = body.result[body.result.length-1].message.text;
+                var from = body.result[body.result.length-1].message.from.id;
+                text = realiseAction(text);
+                console.log( 'from :: ' + from );
+                console.log( 'text :: ' + text );
+
+                sendMessage(from, text);
+            }else{
+                var text = calculateStatus();
+                var from = body.result[body.result.length-1].message.from.id;
+
+                sendMessage(from, text);
+            }
+            
+            // se actualiza la ultima id del último mensaje que se ha recogido
+            lastId = body.result[body.result.length-1].update_id;
         });
     
     }).on('error', (err) => {
         console.log( 'Error : ' + err.message );
     });
+}
+
+function realiseAction(text){
+    switch (text) {
+        case '/comer':
+            feed();
+            return 'HMMMM K RICOOOOO';
+        case '/jugar':
+            play();
+            return 'JAJAJJAA QUE DIVER';
+        case '/dormir':
+            sleep();
+            return 'UII, CUANTO HE DORMIDO';
+
+        default:
+            return calculateStatus();
+    }
+}
+
+
+function calculateStatus(){
+    if( tama.life == 0){
+        return 'estoy muerto';
+    } else if( tama.hungry >= 50 ){
+        return 'tengo hambre';
+    }else if( tama.sleepness >= 50){
+        return 'tengo sueño, quiero dormir';
+    }else if( tama.happiness < 20){
+        return 'estoy triste, juega conmigo';
+    }else if( tama.life <= 50 ){
+        return 'me estoy muriendo';
+    } else {
+        return 'Hola, jajaj, estoy de puta madre';
+    }
 }
 
 function logTamaStatus(){
